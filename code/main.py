e0c8b2bbe21837c7171bdcbe22a3ef6d655cae85
alexeyyumatov <Alex.uma2007@yandex.ru>
2022-12-16 01:47:43 +0500

    pygame window with neural network

--- code/main.py
+++ code/main.py
@@ -1,4 +1,11 @@
 import pygame
+import mediapipe as mp
+import cv2
+
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
 
 if __name__ == '__main__':
     pygame.init()
@@ -6,12 +13,72 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     FPS = 60
+    last_status, hand_type = "", ""
+
+    cap = cv2.VideoCapture(0)
+    w, h = 640, 480
+    cap.set(3, w)
+    cap.set(4, h)
+    cords = {}
 
     running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        pygame.display.flip()
-        clock.tick(FPS)
+    with mp_hands.Hands(max_num_hands=1, min_tracking_confidence=0.9, min_detection_confidence=0.9) as hands:
+        while running:
+
+            success, frame = cap.read()
+            frame = cv2.flip(frame, 1)
+
+            imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = hands.process(imgRGB)
+            imgRGB = cv2.cvtColor(imgRGB, cv2.COLOR_RGB2BGR)
+
+            if results.multi_hand_landmarks:
+                if results.multi_handedness:
+                    hand_type = results.multi_handedness[0].classification[0].label
+
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(imgRGB, hand_landmarks,
+                                              mp_hands.HAND_CONNECTIONS,
+                                              mp_drawing.DrawingSpec(
+                                                  color=(0, 0, 255)),
+                                              mp_drawing.DrawingSpec(
+                                                  color=(0, 0, 0)))
+
+                for id, lm in enumerate(hand_landmarks.landmark):
+                    h, w, c = frame.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    cords[f"{id}"] = cx, cy
+
+                if len(cords) == 21:
+                    if hand_type == "Right":
+                        if int(cords['8'][1]) > int(cords['6'][1]) \
+                                and int(cords['12'][1]) > int(cords['10'][1]) \
+                                and int(cords['16'][1]) > int(cords['14'][1]) \
+                                and int(cords['20'][1]) > int(cords['18'][1]) \
+                                and int(cords['4'][0]) > int(cords['2'][0]):
+                            last_status = "Close"
+                        else:
+                            last_status = "Open"
+
+                    elif hand_type == "Left":
+                        if int(cords['8'][1]) > int(cords['6'][1]) \
+                                and int(cords['12'][1]) > int(cords['10'][1]) \
+                                and int(cords['16'][1]) > int(cords['14'][1]) \
+                                and int(cords['20'][1]) > int(cords['18'][1]) \
+                                and int(cords['4'][0]) < int(cords['2'][0]):
+                            last_status = "Close"
+                        else:
+                            last_status = "Open"
+
+            else:
+                hand_type = "-"
+                last_status = "-"
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    print(last_status, hand_type)
+            pygame.display.flip()
+            clock.tick(FPS)
     pygame.quit()
