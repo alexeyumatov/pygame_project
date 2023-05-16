2c475ad72301a5ed51a52728223996500e09f878
VeregaS <sergeiusenkov56@gmail.com>
2023-05-16 13:13:19 +0500

    hands presentation version

--- hands/hands.py
+++ hands/hands.py
@@ -1,46 +1,25 @@
 import cv2
 import mediapipe as mp
-import time
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 
 cap = cv2.VideoCapture(0)
-w, h = 1280, 720
+w, h = 640, 480
 cap.set(3, w)
 cap.set(4, h)
 
 cords = {}
-pTime = 0
 last_status, hand_type = "", ""
 
-# Вот тут параметры со значениями 0.9 и 0.9 эти значения скорее всего надо будет в настройки запихнуть
-# т.к они отвечают за чувствительность нейронки к опознаванию рук и тп
-# у всех вебки разные у кого то нормальные у кого то нет и по этому эту прогу надо в игру вшить для теста вебки и её калибровки
+
 with mp_hands.Hands(max_num_hands=1, min_tracking_confidence=0.9, min_detection_confidence=0.9) as hands:
     while True:
-
         # ------------------------- Camera Load --------------------- #
         success, frame = cap.read()
         frame = cv2.flip(frame, 1)
         # ----------------------------------------------------------- #
-
-        ############################# fps #############################
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-        cv2.putText(frame, str(int(fps)), (10, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-
-        cv2.putText(frame, f"Hand: {hand_type}", (10, 70),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-
-        cv2.putText(frame, f"Status: {last_status}",
-                    (10, 110),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-        #############################################################
-
         imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = hands.process(imgRGB)
         imgRGB = cv2.cvtColor(imgRGB, cv2.COLOR_RGB2BGR)
@@ -68,6 +47,15 @@ with mp_hands.Hands(max_num_hands=1, min_tracking_confidence=0.9, min_detection_
                     cx, cy = int(lm.x * w), int(lm.y * h)
                     cords[f"{id}"] = cx, cy
                 # ----------------------------------------------------------- #
+                # ########################### TEXT ########################## #
+                if len(cords) == 21:
+                    cv2.putText(imgRGB, f"Hand: {hand_type}", (cords['0'][0], cords['0'][1] + 35),
+                                cv2.FONT_HERSHEY_DUPLEX, 0.85, (0, 0, 0), 1)
+
+                    cv2.putText(imgRGB, f"Status: {last_status}",
+                                (cords['0'][0], cords['0'][1] + 75),
+                                cv2.FONT_HERSHEY_DUPLEX, 0.85, (0, 0, 0), 1)
+                # ########################################################## #
 
                 # ######################## Hand_Status ###################### #
                 if len(cords) == 21:
